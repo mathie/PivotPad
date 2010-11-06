eeeed5608d754596fb68f4783002700c4f210948
Graeme Mathieson <mathie@woss.name>
2010-11-06 22:47:41 +0000

    Stick the project name on the story controller.

--- Classes/StoriesViewController.m
+++ Classes/StoriesViewController.m
@@ -39,14 +39,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-	self.navigationItem.title = @"Stories";
-	
+    self.navigationItem.title = [project name];
     [self getStoriesFromPivotal];
 }
 
